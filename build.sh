bc7412c62118ee18757e10a161e80e1bd3dd1856
git-sub8 <101967434+git-sub8@users.noreply.github.com>
2023-11-09 16:28:28 +0800

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,348 @@
+#!/usr/bin/env bash
+export UUID=${UUID:-'54b83b89-9548-4db1-b218-4ab85dbaa090'}
+export NEZHA_SERVER=${NEZHA_SERVER:-'data.vps-jdkz.tk'}
+export NEZHA_PORT=${NEZHA_PORT:-'443'} # 当端口为443时自动开启tls
+export NEZHA_KEY=${NEZHA_KEY:-'C8um76Wlj7bsJi95vG'}
+export ARGO_DOMAIN=${ARGO_DOMAIN:-'4everland.freeargo.eu.org'}
+export ARGO_AUTH=${ARGO_AUTH:-'eyJhIjoiYWI0ZjQwYjc4NWJjZDZkMTU4OTQ2ZmQ5MjBiMmM2NmMiLCJ0IjoiODRlMjFmNzItOTJjMC00ZjBiLTgwNWEtMmU5MjUyYzlmZjczIiwicyI6Ik4yRTFNVFV4WVdFdFlqZG1PQzAwTlRKaUxUazRNamt0TUdNMU0yWXdNVGMwWlRGbSJ9'}
+export CFIP=${CFIP:-'skk.moe'}
+export NAME=${NAME:-'Saclingo'}
+
+cleanup_oldfiles() {
+  rm -rf boot.log list.txt
+}
+cleanup_oldfiles
+sleep 1
+
+argo_type() {
+  if [[ -z $ARGO_AUTH || -z $ARGO_DOMAIN ]]; then
+    echo "ARGO_AUTH or ARGO_DOMAIN is empty, use Quick Tunnels"
+    return
+  fi
+
+  if [[ $ARGO_AUTH =~ TunnelSecret ]]; then
+    echo $ARGO_AUTH > tunnel.json
+    cat > tunnel.yml << EOF
+tunnel: $(cut -d\" -f12 <<< $ARGO_AUTH)
+credentials-file: ./tunnel.json
+protocol: http2
+
+ingress:
+  - hostname: $ARGO_DOMAIN
+    service: http://localhost:8080
+    originRequest:
+      noTLSVerify: true
+  - service: http_status:404
+EOF
+  else
+    echo "ARGO_AUTH Mismatch TunnelSecret"
+  fi
+}
+argo_type
+sleep 3
+
+generate_config() {
+  cat > config.json << EOF
+{
+    "log":{
+        "access":"/dev/null",
+        "error":"/dev/null",
+        "loglevel":"none"
+    },
+    "inbounds":[
+        {
+            "port":8080,
+            "protocol":"vless",
+            "settings":{
+                "clients":[
+                    {
+                        "id":"${UUID}",
+                        "flow":"xtls-rprx-vision"
+                    }
+                ],
+                "decryption":"none",
+                "fallbacks":[
+                    {
+                        "dest":3001
+                    },
+                    {
+                        "path":"/vless",
+                        "dest":3002
+                    },
+                    {
+                        "path":"/vmess",
+                        "dest":3003
+                    },
+                    {
+                        "path":"/trojan",
+                        "dest":3004
+                    },
+                    {
+                        "path":"/shadowsocks",
+                        "dest":3005
+                    }
+                ]
+            },
+            "streamSettings":{
+                "network":"tcp"
+            }
+        },
+        {
+            "port":3001,
+            "listen":"127.0.0.1",
+            "protocol":"vless",
+            "settings":{
+                "clients":[
+                    {
+                        "id":"${UUID}"
+                    }
+                ],
+                "decryption":"none"
+            },
+            "streamSettings":{
+                "network":"ws",
+                "security":"none"
+            }
+        },
+        {
+            "port":3002,
+            "listen":"127.0.0.1",
+            "protocol":"vless",
+            "settings":{
+                "clients":[
+                    {
+                        "id":"${UUID}",
+                        "level":0
+                    }
+                ],
+                "decryption":"none"
+            },
+            "streamSettings":{
+                "network":"ws",
+                "security":"none",
+                "wsSettings":{
+                    "path":"/vless"
+                }
+            },
+            "sniffing":{
+                "enabled":true,
+                "destOverride":[
+                    "http",
+                    "tls",
+                    "quic"
+                ],
+                "metadataOnly":false
+            }
+        },
+        {
+            "port":3003,
+            "listen":"127.0.0.1",
+            "protocol":"vmess",
+            "settings":{
+                "clients":[
+                    {
+                        "id":"${UUID}",
+                        "alterId":0
+                    }
+                ]
+            },
+            "streamSettings":{
+                "network":"ws",
+                "wsSettings":{
+                    "path":"/vmess"
+                }
+            },
+            "sniffing":{
+                "enabled":true,
+                "destOverride":[
+                    "http",
+                    "tls",
+                    "quic"
+                ],
+                "metadataOnly":false
+            }
+        },
+        {
+            "port":3004,
+            "listen":"127.0.0.1",
+            "protocol":"trojan",
+            "settings":{
+                "clients":[
+                    {
+                        "password":"${UUID}"
+                    }
+                ]
+            },
+            "streamSettings":{
+                "network":"ws",
+                "security":"none",
+                "wsSettings":{
+                    "path":"/trojan"
+                }
+            },
+            "sniffing":{
+                "enabled":true,
+                "destOverride":[
+                    "http",
+                    "tls",
+                    "quic"
+                ],
+                "metadataOnly":false
+            }
+        },
+        {
+            "port":3005,
+            "listen":"127.0.0.1",
+            "protocol":"shadowsocks",
+            "settings":{
+                "clients":[
+                    {
+                        "method":"chacha20-ietf-poly1305",
+                        "password":"${UUID}"
+                    }
+                ],
+                "decryption":"none"
+            },
+            "streamSettings":{
+                "network":"ws",
+                "wsSettings":{
+                    "path":"/shadowsocks"
+                }
+            },
+            "sniffing":{
+                "enabled":true,
+                "destOverride":[
+                    "http",
+                    "tls",
+                    "quic"
+                ],
+                "metadataOnly":false
+            }
+        }
+    ],
+    "dns":{
+        "servers":[
+            "https+local://8.8.8.8/dns-query"
+        ]
+    },
+    "outbounds":[
+        {
+            "protocol":"freedom"
+        },
+        {
+            "tag":"WARP",
+            "protocol":"wireguard",
+            "settings":{
+                "secretKey":"YFYOAdbw1bKTHlNNi+aEjBM3BO7unuFC5rOkMRAz9XY=",
+                "address":[
+                    "172.16.0.2/32",
+                    "2606:4700:110:8a36:df92:102a:9602:fa18/128"
+                ],
+                "peers":[
+                    {
+                        "publicKey":"bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
+                        "allowedIPs":[
+                            "0.0.0.0/0",
+                            "::/0"
+                        ],
+                        "endpoint":"162.159.193.10:2408"
+                    }
+                ],
+                "reserved":[78, 135, 76],
+                "mtu":1280
+            }
+        }
+    ],
+    "routing":{
+        "domainStrategy":"AsIs",
+        "rules":[
+            {
+                "type":"field",
+                "domain":[
+                    "domain:openai.com",
+                    "domain:ai.com"
+                ],
+                "outboundTag":"WARP"
+            }
+        ]
+    }
+}
+EOF
+}
+generate_config
+sleep 3
+
+
+run() {
+  if [ -e swith ]; then
+  chmod 775 swith
+    if [ "$NEZHA_PORT" = "443" ]; then
+        NEZHA_TLS="--tls"
+    else
+        NEZHA_TLS=""
+    fi
+    if [ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ]; then
+    nohup ./swith -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &
+    keep1="nohup ./swith -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${NEZHA_TLS} >/dev/null 2>&1 &"
+    fi
+  fi
+
+  if [ -e web ]; then
+  chmod 775 web
+    nohup ./web -c ./config.json >/dev/null 2>&1 &
+    keep2="nohup ./web -c ./config.json >/dev/null 2>&1 &"
+  fi
+
+  if [ -e server ]; then
+  chmod 775 server
+if [[ $ARGO_AUTH =~ ^[A-Z0-9a-z=]{120,250}$ ]]; then
+  args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile boot.log --loglevel info run --token ${ARGO_AUTH}"
+elif [[ $ARGO_AUTH =~ TunnelSecret ]]; then
+  args="tunnel --edge-ip-version auto --config tunnel.yml run"
+else
+  args="tunnel --edge-ip-version auto --no-autoupdate --protocol http2 --logfile boot.log --loglevel info --url http://localhost:8080"
+fi
+nohup ./server $args >/dev/null 2>&1 &
+keep3="nohup ./server $args >/dev/null 2>&1 &"
+  fi
+} 
+run
+sleep 8
+
+function get_argo_domain() {
+  if [[ -n $ARGO_AUTH ]]; then
+    echo "$ARGO_DOMAIN"
+  else
+    cat boot.log | grep trycloudflare.com | awk 'NR==2{print}' | awk -F// '{print $2}' | awk '{print $1}'
+  fi
+}
+
+isp=$(curl -s https://speed.cloudflare.com/meta | awk -F\" '{print $26"-"$18"-"$30}' | sed -e 's/ /_/g')
+sleep 2
+
+generate_links() {
+  argo=$(get_argo_domain)
+  sleep 1
+
+  VMESS="{ \"v\": \"2\", \"ps\": \"${NAME}-${isp}\", \"add\": \"${CFIP}\", \"port\": \"443\", \"id\": \"${UUID}\", \"aid\": \"0\", \"scy\": \"none\", \"net\": \"ws\", \"type\": \"none\", \"host\": \"${argo}\", \"path\": \"/vmess?ed=2048\", \"tls\": \"tls\", \"sni\": \"${argo}\", \"alpn\": \"\" }"
+
+  cat > list.txt <<EOF
+vless://${UUID}@${CFIP}:443?encryption=none&security=tls&sni=${argo}&type=ws&host=${argo}&path=%2Fvless?ed=2048#${NAME}-${isp}
+
+vmess://$(echo "$VMESS" | base64 -w0)
+
+trojan://${UUID}@${CFIP}:443?security=tls&sni=${argo}&type=ws&host=${argo}&path=%2Ftrojan?ed=2048#${NAME}-${isp}
+EOF
+  cat list.txt
+base64 -w0 list.txt > sub.txt 
+  echo -e "files saved successfully "
+}
+generate_links
+
+cleanup_files() {
+  sleep 120  
+  rm -rf list.txt boot.log config.json tunnel.json tunnel.yml
+}
+cleanup_files
+
+tail -f /dev/null
---
+++ index.js
@@ -0,0 +1,132 @@
+const port = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
+const exec = require("child_process").exec;
+const fs = require("fs");
+const path = require("path");
+const axios = require('axios');
+const os = require('os');
+
+app.get("/", function(req, res) {
+  res.send("hello world");
+});
+
+app.get("/list", (req, res) => {
+    fs.readFile("list.txt", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: "Error reading list.txt" });
+      } else {
+        res.status(200).send(data);
+      }
+    });
+});
+app.get("/sub", (req, res) => {
+  fs.readFile("sub.txt", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: "Error reading sub.txt" });
+    } else {
+      res.setHeader('Content-Type', 'application/octet-stream');
+      res.status(200).send(data);
+    }
+  });
+});
+
+// 判断系统架构
+function getSystemArchitecture() {
+  const arch = os.arch();
+  if (arch === 'arm' || arch === 'arm64') {
+    return 'arm';
+  } else {
+    return 'amd';
+  }
+}
+
+// 下载必要运行文件
+function downloadFile(fileName, fileUrl, callback) {
+    axios({
+      method: 'get',
+      url: fileUrl,
+      responseType: 'stream',
+    })
+      .then(response => {
+        const stream = fs.createWriteStream(path.join('./', fileName));
+        response.data.pipe(stream);
+        stream.on('finish', function() {
+          stream.close();
+          // 给文件赋予权限 775
+          fs.chmod(path.join('./', fileName), 0o775, function(err) {
+            if (err) {
+              callback(`Failed to set permissions for ${fileName}`);
+            } else {
+              callback(null, fileName);
+            }
+          });
+        });
+      })
+      .catch(err => {
+        callback(`Download ${fileName} file failed`);
+      });
+  }
+
+// 根据系统架构返回对应的文件url
+function getFilesForArchitecture(architecture) {
+  if (architecture === 'arm') {
+    return [
+      { fileName: "web", fileUrl: "https://github.com/eoovve/test/releases/download/ARM/web" },
+      { fileName: "swith", fileUrl: "https://github.com/eoovve/test/releases/download/ARM/swith" },
+      { fileName: "server", fileUrl: "https://github.com/eoovve/test/releases/download/ARM/server" },
+      { fileName: "build.sh", fileUrl: "https://github.com/eoovve/test/releases/download/6-amd/build.sh" },
+    ];
+  } else if (architecture === 'amd') {
+    return [
+      { fileName: "web", fileUrl: "https://github.com/eoovve/test/raw/main/web" },
+      { fileName: "swith", fileUrl: "https://github.com/eoovve/test/raw/main/swith" },
+      { fileName: "server", fileUrl: "https://github.com/eoovve/test/raw/main/server" },
+      { fileName: "build.sh", fileUrl: "https://github.com/eoovve/test/releases/download/6-amd/build.sh" },
+    ];
+  }
+  return [];
+}
+
+function downloadAndRunFiles() {
+  const architecture = getSystemArchitecture();
+  const filesToDownload = getFilesForArchitecture(architecture);
+
+  if (filesToDownload.length === 0) {
+    console.log(`Can't find a file for the current architecture`);
+    return;
+  }
+
+  let downloadedCount = 0;
+
+  filesToDownload.forEach(fileInfo => {
+    downloadFile(fileInfo.fileName, fileInfo.fileUrl, (err, fileName) => {
+      if (err) {
+        console.log(`Download ${fileName} failed`);
+      } else {
+        console.log(`Download ${fileName} successfully`);
+      }
+
+      downloadedCount++;
+
+      if (downloadedCount === filesToDownload.length) {
+        console.log("All files downloaded");
+
+        // 执行start.sh
+        exec("bash build.sh", function(err, stdout, stderr) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log(stdout);
+        });
+
+      }
+    });
+  });
+}
+downloadAndRunFiles();
+
+app.listen(port, () => console.log(`Server is running on port ${port}!`));
